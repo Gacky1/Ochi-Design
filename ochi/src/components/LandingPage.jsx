b0abf6fbbf41ef8a2d2f35f2205518d53ffca3d3
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-02-26 00:07:59 +0530

    Landing page commited

---
+++ ochi/src/components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className="textstructure mt-40 px-20">
+            <div className="masker">
+                <h1 className='uppercase text-9xl leading-[7vw] tracking-tighter font-["Founders Grotesk"] font-medium'>We Create</h1>
+            </div>
+            <div className="masker">
+                <h1 className='uppercase text-9xl leading-[7vw] tracking-tighter font-["Founders Grotesk"] font-medium'>eye-opening</h1>
+            </div>
+            <div className="masker">
+                <h1 className='uppercase text-9xl leading-[7vw] tracking-tighter font-["Founders Grotesk"] font-medium'>Presentations</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
